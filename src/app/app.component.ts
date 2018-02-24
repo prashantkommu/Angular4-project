aa736919d29765d09508a1b8d4e24efe1ded60b6
Mohammad Irfan <33131617+irfanfarhan@users.noreply.github.com>
2018-02-24 10:31:31 +0530

    Commit on Added Plugins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators
+} from '@angular/forms';
+import { InputTextModule, ButtonModule } from 'primeng/primeng';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  processingInfoForm: FormGroup;
+  processingInfoData: any = [];
+  constructor(private _fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.processingInfoForm = this._fb.group({
+      quantity: [''],
+      billedDaysSupply: [''],
+      actualDaysSupply: ['']
+    });
+    this.processingInfoData['quantity'] = '0005';
+    this.processingInfoData['billedDaysSupply'] = '03';
+    this.processingInfoData['actualDaysSupply'] = '2';
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { InputTextModule, ButtonModule } from 'primeng/primeng';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
