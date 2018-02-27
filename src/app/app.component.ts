354c8ef3da826fbadfbbe4a6f74526c12c51bd65
Mohammad Irfan <33131617+irfanfarhan@users.noreply.github.com>
2018-02-28 00:30:34 +0530

    add dropdowns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import {
   FormControl,
   Validators
 } from '@angular/forms';
-import { InputTextModule, ButtonModule } from 'primeng/primeng';
+import { InputTextModule, ButtonModule, DropdownModule } from 'primeng/primeng';
 
 @Component({
   selector: 'app-root',
@@ -14,8 +14,21 @@ import { InputTextModule, ButtonModule } from 'primeng/primeng';
 })
 export class AppComponent implements OnInit {
   processingInfoForm: FormGroup;
+  processingInfoMainForm: FormGroup;
   processingInfoData: any = [];
-  constructor(private _fb: FormBuilder) { }
+  processingInfoMainData: any = [];
+  fill: any;
+  primary: any;
+  secondPayor: any;
+  processAction: any;
+  fillData: any[] = [
+    { name: 'New York', code: 'NY' },
+    { name: 'Rome', code: 'RM' },
+    { name: 'London', code: 'LDN' },
+    { name: 'Istanbul', code: 'IST' },
+    { name: 'Paris', code: 'PRS' }
+  ];
+  constructor(private _fb: FormBuilder) {}
 
   ngOnInit() {
     this.processingInfoForm = this._fb.group({
@@ -23,10 +36,14 @@ export class AppComponent implements OnInit {
       billedDaysSupply: [''],
       actualDaysSupply: ['']
     });
+    this.processingInfoMainForm = this._fb.group({
+      fill: [''],
+      primary: [''],
+      secondPayor: [''],
+      processAction: ['']
+    });
     this.processingInfoData['quantity'] = '0005';
     this.processingInfoData['billedDaysSupply'] = '03';
     this.processingInfoData['actualDaysSupply'] = '2';
   }
-
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { InputTextModule, ButtonModule } from 'primeng/primeng';
+import { InputTextModule, ButtonModule, DropdownModule } from 'primeng/primeng';
 
 @NgModule({
   declarations: [
@@ -15,7 +15,7 @@ import { InputTextModule, ButtonModule } from 'primeng/primeng';
     FormsModule,
     ReactiveFormsModule,
     InputTextModule,
-    ButtonModule
+    ButtonModule, DropdownModule
   ],
   providers: [],
   bootstrap: [AppComponent]
